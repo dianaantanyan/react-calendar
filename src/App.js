8cd464bbd762297387712c8998c34ab1e6bd154b
dianaantanyan <dianch.antanyann@gmail.com>
2023-06-29 18:37:22 +0400

    Calendar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calendar from './components/Calender';
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
